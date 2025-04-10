80776350e19767197888f03e6bd1bbe5140e5ea1
nandurwit <84932159+nandurwit@users.noreply.github.com>
2025-04-10 11:15:58 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
-<?php
-session_start();
+<!DOCTYPE html>
+<html lang="en">
 
 // Array of updated technology-related images and descriptions from Pexels and Pixabay with smaller resolution images
 $tech_images = [
